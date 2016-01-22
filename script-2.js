1c09769b92abcf0108aa3c635c68c3c591c91da4
Poloz <underpoloz@gmail.com>
2016-01-22 22:52:42 +0300

    cal

---
+++ script-2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by A on 18.01.2016.
+ */
+
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+} else {
+    alert('The File APIs are not fully supported in this browser.');
+}
